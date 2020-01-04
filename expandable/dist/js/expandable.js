fbac98d3b937dbf665db4dd5ce4f77aede48c828
Zakiya <zakiya@chapterthree.com>
2020-01-03 20:09:30 -0800

    Add action.

--- expandable/dist/js/expandable.js
+++ expandable/dist/js/expandable.js
@@ -95,6 +95,40 @@
 
 document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
 
+function addClass(el, klass) {
+  el.classList.add(klass);
+}
+
+function removeClass(el, klass) {
+  el.classList.remove(klass);
+}
+
+var accordionItems = document.querySelectorAll('li');
+var accordionContentPanes = document.querySelectorAll('.accordion__body'); // Show first by default
+
+accordionItems[0].querySelector('.accordion__body').classList.remove('hidden'); // Hide each besides the targeted accordion on click
+
+accordionItems.forEach(function (accordion) {
+  // Clicked accordions clickable target
+  var accordionTitleRow = accordion.firstElementChild;
+  accordionTitleRow.addEventListener('click', toggleAccordion); // console.log(accordion.firstElementChild);
+});
+
+function toggleAccordion(e) {
+  accordionContentPanes.forEach(function (content) {
+    // Check if clicked row matches the content's previous element sibling
+    console.log(content);
+
+    if (content.previousElementSibling === e.target) {
+      removeClass(content, 'hidden');
+      addClass(content.parentElement, 'active');
+    } else {
+      removeClass(content.parentElement, 'active');
+      addClass(content, 'hidden');
+    }
+  });
+}
+
 /***/ }),
 
 /***/ "./src/sass/style.scss":
