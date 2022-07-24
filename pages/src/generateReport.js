cde86736ff5b61f15a076e175d8b957c62b1ad15
zakiya <zakiya@chapterthree.com>
2022-07-24 16:34:26 -0700

    Better report.

--- pages/src/generateReport.js
+++ pages/src/generateReport.js
@@ -1,22 +1,12 @@
-import { createRequire } from "module";
 import { readFileSync, writeFile } from "node:fs";
-const require = createRequire(import.meta.url);
 import { fileOutputName } from "./shared.js";
 
-const file = readFileSync(fileOutputName, "utf8");
-const rawData = JSON.parse(file);
+const rawDataFile = readFileSync(fileOutputName, "utf8");
+const rawData = JSON.parse(rawDataFile);
 
-const memberships = [
-  "2022 Epic Membership",
-  "2022 Mythic Membership",
-  "Manual Epic Membership",
-];
-const bandanas = ["Classic Sirens Bandana (Ready to Ship)"];
-const tshirts = [
-  "Classic Sirens T-shirt (Pre-Order)",
-  "2022 Exclusive: OH WAVE T-shirt (Pre-Order)",
-];
-const stickers = ["Sirens Sticker Pack (Ready to Ship)"];
+const categoriesFile = readFileSync("src/categories.json", "utf8");
+const categories = JSON.parse(categoriesFile);
+
 const others = [
   'Free; Fulfilled @ Jeralyn"',
   'Cash to Zakiya then venmo @ Home & Away"',
@@ -31,50 +21,54 @@ const others = [
 ];
 
 let output = "";
-let membershipCount = 0;
-let bandanaCount = 0;
-let tshirtCount = 0;
-let stickerCount = 0;
 let otherText = "";
 
-rawData.forEach((element) => {
-  if (memberships.includes(element.Lineitemname)) {
-    membershipCount = membershipCount + parseInt(element.Lineitemquantity);
-  } else if (bandanas.includes(element.Lineitemname)) {
-    bandanaCount = bandanaCount + parseInt(element.Lineitemquantity);
-  } else if (tshirts.includes(element.Lineitemname)) {
-    tshirtCount = tshirtCount + parseInt(element.Lineitemquantity);
-  } else if (stickers.includes(element.Lineitemname)) {
-    stickerCount = stickerCount + parseInt(element.Lineitemquantity);
-  } else if (others.includes(element.OrderID)) {
-    // Known efemera. Do not print.
-  } else {
+rawData.forEach((order) => {
+  let orderType = null;
+
+  categories.forEach((category) => {
+    for (const [key, product] of Object.entries(category.products)) {
+      if (product.label === order.Lineitemname) {
+        category.categoryCount += parseInt(order.Lineitemquantity);
+        category.products[key].productCount += parseInt(order.Lineitemquantity);
+        orderType = "known";
+      } else {
+        orderType = "unknown";
+      }
+    }
+  });
+
+  if (!others.includes(order.OrderID) && orderType === null) {
     otherText += "\n needs review:";
-    otherText += "\n " + element.OrderID;
-    otherText += "\n " + element.Email;
-    otherText += "\n " + element.Lineitemname;
+    otherText += "\n " + order.OrderID;
+    otherText += "\n " + order.Email;
+    otherText += "\n " + order.Lineitemname;
   }
 });
 
-const timeStamp = new Date().toLocaleDateString("en-US", {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
+categories.forEach((category) => {
+  for (const [key, product] of Object.entries(category.products)) {
+    output += `<h4>${product.label}: ${product.productCount}</h4>`;
+  }
+  output += `<h2>Total ${category.type}: ${category.categoryCount}</h4>`;
 });
 
-output += "<h4>Memberships</h4> ";
-output += membershipCount;
-output += "<h4>Bandanas</h4> ";
-output += bandanaCount;
-output += "<h4>Tshirts </h4>";
-output += tshirtCount;
-output += "<h4>Stickers </h4>";
-output += stickerCount;
+const timeStamp = new Date().toLocaleDateString("en-US", {
+  dateStyle: "medium",
+  timeStyle: "medium",
+});
+
 output += otherText;
 
 const template = `<html>
 <head>
+<style>
+h2 {
+background-color: #0A2141;
+color: #FFFFFF;
+padding: 10px;
+}
+</style>
 </head>
 <body>
 
@@ -88,4 +82,4 @@ ${timeStamp}
 </body>
 </html>`;
 
-writeFile("index.html", template, (error) => {});
+writeFile("sirens-merch.html", template, (error) => {});
