8c2966973ee14bf76b2d1d74495bb69c05c1d57d
zakiya <zakiya@chapterthree.com>
2023-01-28 19:09:49 -0800

    cart and membembership page

--- pages/src/membership/membership.js
+++ pages/src/membership/membership.js
@@ -1,18 +1,77 @@
-{
-  /* <script src="membership.js" type="text/javascript">
+const sirensQuantityHideClass = "sirens-quantity-wrapper-hide";
+const sirensEpicLabel = '[aria-label="Quantity, 2023 Epic Membership"]';
+const subTotalClass = "cart-subtotal";
 
-</script> */
-}
-const sirensID = "sirens-membership";
-const hideQuantityInput = () => {
-  const el = document.getElementById(sirensID);
-  const wrapper = el.parentElement.parentElement.parentElement;
-  wrapper.classList.add("sirens-membership-wrapper");
-  console.log(sirensID);
-  console.log(wrapper);
-  console.log(wrapper.classList);
+// Utility function;
+const waitForElm = (selector) => {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
 };
 
-if (document.getElementsByClassName(sirensID)) {
-  hideQuantityInput();
-}
+// Hide input at /memberships.
+const hideBlockQuantityInput = () => {
+  const sirensID = "sirens-membership";
+
+  if (document.getElementById(sirensID)) {
+    const blockEl = document.getElementById(sirensID);
+    blockEl.parentElement.parentElement.parentElement.classList.add(
+      sirensQuantityHideClass
+    );
+  }
+};
+
+// Hide input at /cart.
+const hideCartQuantityInput = (sirensEpicLabel) => {
+  if (document.querySelectorAll(sirensEpicLabel).length > 0) {
+    const cartQuantityInputs = document.querySelectorAll(sirensEpicLabel);
+    cartQuantityInputs.forEach((input) => {
+      input.parentElement.parentElement.classList.add(sirensQuantityHideClass);
+    });
+  }
+};
+
+// Add membership button;
+const addAnotherMembership = () => {
+  if (document.getElementsByClassName(subTotalClass).length > 0) {
+    const summaryClass = "cart-summary";
+    const addButtonClasses = [
+      "sqs-button-element--primary",
+      "sirens-add-membership",
+    ];
+    const addButtonText = "Add another membership";
+
+    const textNode = document.createTextNode(addButtonText);
+
+    const addAnotherButton = document.createElement("a");
+    addAnotherButton.classList.add(addButtonClasses[0]);
+    addAnotherButton.classList.add(addButtonClasses[1]);
+    addAnotherButton.setAttribute("href", "/memberships");
+    addAnotherButton.appendChild(textNode);
+
+    const summary = document.getElementsByClassName(summaryClass);
+    summary[0].insertBefore(addAnotherButton, summary[0].childNodes[1]);
+  }
+};
+
+// Run functions.
+hideBlockQuantityInput();
+
+waitForElm(sirensEpicLabel).then(() => {
+  hideCartQuantityInput(sirensEpicLabel);
+  addAnotherMembership();
+});
