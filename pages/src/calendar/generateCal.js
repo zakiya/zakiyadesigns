218318ab38b5dd35ff416e119e8ae933ca991742
zakiya <zakiya@chapterthree.com>
2024-04-18 19:07:59 -0400

    Use variable for iframe.

--- pages/src/calendar/generateCal.js
+++ pages/src/calendar/generateCal.js
@@ -8,9 +8,13 @@ import {template} from "./template.js";
 
 const oldURL = {
     "height": 700,
+    "src1": "dnBzZWMuc2lyZW5zQGdtYWlsLmNvbQ",
+    "src2": "M2FiOGxsZXRqNjB2MTF1OGNyYzIwNnJhbXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+    "color1": "3F51B5",
+    "color2": "039BE5",
 }
 
-let output = `<iframe src="https://calendar.google.com/calendar/embed?height${oldURL.height}&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showDate=1&showPrint=0&src=dnBzZWMuc2lyZW5zQGdtYWlsLmNvbQ&src=M2FiOGxsZXRqNjB2MTF1OGNyYzIwNnJhbXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5&color=%23039BE5" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+let output = `<iframe src="https://calendar.google.com/calendar/embed?height${oldURL.height}&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showDate=1&showPrint=0&src=${oldURL.src1}&src=${oldURL.src2}&color=%23${oldURL.color1}&color=%23${oldURL.color2}" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 `;
 
 writeFile("./src/calendar/calendar.html", template(output), (error) => {
