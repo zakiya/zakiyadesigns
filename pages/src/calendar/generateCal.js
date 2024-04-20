875a36b619deebbfb351c3b44d4b7f84d6f3f49e
zakiya <zakiya@chapterthree.com>
2024-04-19 23:20:27 -0400

    1 line per varialbe for readability and editability

--- pages/src/calendar/generateCal.js
+++ pages/src/calendar/generateCal.js
@@ -4,12 +4,10 @@ import {template} from "./template.js";
 
 const vars = {
     "height": 600,
-    "srcOldSirens": "Y2FsZW5kYXJAc2FuZGllZ29zaXJlbnMuY29t",
-    "srcUSWNT": "Y2FsZW5kYXJAc2FuZGllZ29zaXJlbnMuY29t",
     "srcNewSirens": "Y2FsZW5kYXJAc2FuZGllZ29zaXJlbnMuY29t",
-    "color1": "3F51B5",
-    "color2": "039BE5",
-    "color3": "8B03E5",
+    "srcUSWNT": "jsrdpoh8m3n0ktnignum3ckfpc1mu5k1%40import.calendar.google.com",
+    "color1": "230B8043",
+    "color2": "233F51B5",
 }
 
 const buildiFrames = (device) => {
@@ -17,11 +15,17 @@ const buildiFrames = (device) => {
     const agenda = (device == "phone") ? `&mode=AGENDA` : ``;
     const css = (device == "phone") ? `visible` : `hidden`;
 
-    const output = `
+    let output = `
     <div class="phone-${css} responsive-iframe-container">
-        <iframe src="https://calendar.google.com/calendar/embed?height=${vars.height}&wkst=1&bgcolor=%23ffffff${agenda}&ctz=America%2FLos_Angeles&showDate=1&showPrint=0&src=${vars.srcOldSirens}&src=${vars.srcNewSirens}&src=${vars.srcUSWNT}&color=%23${vars.color2}&color=%23${vars.color2}&color=%23${vars.color2}" style="border-width:0" width="800" height="${vars.height}" frameborder="0" scrolling="no"></iframe>
-    </div>
-    `
+        <iframe src="https://calendar.google.com/calendar/embed?height=${vars.height}&wkst=1&bgcolor=%23ffffff${agenda}&ctz=America%2FLos_Angeles&showDate=1&showPrint=0`
+    output += ``;
+    output += ``;
+    output += `&src=${vars.srcNewSirens}`;
+    output += `&src=${vars.srcUSWNT}`;
+    output += `&color=%${vars.color1}`;
+    output += `&color=%${vars.color2}`;
+    output += `" style="border-width:0" width="800" height="${vars.height}" frameborder="0" scrolling="no"></iframe>`;
+    output += `</div>`;
 
     return output;
 };
