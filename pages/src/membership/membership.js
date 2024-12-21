26e08848d522a5199285a849799d7cd59a374425
zakiya <zoochia@gmail.com>
2024-12-21 15:07:29 -0800

    Update Sirens membership js for 2025.

--- pages/src/membership/membership.js
+++ pages/src/membership/membership.js
@@ -1,5 +1,7 @@
 const sirensQuantityHideClass = "sirens-quantity-wrapper-hide";
-const sirensEpicLabel = '[aria-label="Quantity, 2023 Epic Membership"]';
+// In a new year, change the string, not the variable
+const sirensEpicLabel = '[aria-label="Quantity, 2025 Epic Membership"]';
+const sirensEpicShortLabel = '[aria-label="2025 Epic Membership"]';
 const subTotalClass = "cart-subtotal";
 
 // Utility function;
@@ -23,7 +25,7 @@ const waitForElm = (selector) => {
   });
 };
 
-// Hide input at /memberships.
+// Hide quantity input at /memberships.
 const hideBlockQuantityInput = () => {
   const sirensID = "sirens-membership";
   const cartSelector = ".sqs-add-to-cart-button-wrapper";
@@ -34,7 +36,13 @@ const hideBlockQuantityInput = () => {
     const blockEls = document.getElementsByClassName(sirensID);
 
     Array.from(blockEls).forEach((blockEl) => {
-      blockEl.parentElement.parentElement.parentElement.classList.add(
+      // Step through each parentElement
+      //          div.sirens-membership                <-embedded
+      //       div.sqs-block-content
+      //    div.sqs-block-code
+      // div.fe-block.sirens-quantity-wrapper-hide    <----added
+
+      blockEl.parentElement.parentElement.parentElement.parentElement.classList.add(
         sirensQuantityHideClass
       );
     });
@@ -44,7 +52,6 @@ const hideBlockQuantityInput = () => {
       let helpTextEl = document.createElement("div");
 
       helpTextEl.appendChild(document.createTextNode(helpText));
-
       helpTextEl.classList.add(addButtonClasses[0]);
 
       item.appendChild(helpTextEl);
@@ -52,13 +59,21 @@ const hideBlockQuantityInput = () => {
   }
 };
 
-// Hide input at /cart.
+// Hide quantity input at /cart.
 const hideCartQuantityInput = (sirensEpicLabel) => {
   if (document.querySelectorAll(sirensEpicLabel).length > 0) {
     const cartQuantityInputs = document.querySelectorAll(sirensEpicLabel);
     cartQuantityInputs.forEach((input) => {
       input.parentElement.parentElement.classList.add(sirensQuantityHideClass);
     });
+  }
+};
+
+// Change link on scarf images at /store.
+const changeStoreLink = (sirensEpicShortLabel) => {
+  if (document.querySelectorAll(sirensEpicShortLabel).length > 0) {
+    const storeLink = document.querySelectorAll(sirensEpicShortLabel);
+    storeLink[0].setAttribute("href", "/membership");
   }
 };
 
@@ -77,7 +92,7 @@ const addAnotherMembership = () => {
     const addAnotherButton = document.createElement("a");
     addAnotherButton.classList.add(addButtonClasses[0]);
     addAnotherButton.classList.add(addButtonClasses[1]);
-    addAnotherButton.setAttribute("href", "/memberships");
+    addAnotherButton.setAttribute("href", "/membership");
     addAnotherButton.appendChild(textNode);
 
     const summary = document.getElementsByClassName(summaryClass);
@@ -91,4 +106,8 @@ hideBlockQuantityInput();
 waitForElm(sirensEpicLabel).then(() => {
   hideCartQuantityInput(sirensEpicLabel);
   addAnotherMembership();
+});
+
+waitForElm(sirensEpicShortLabel).then(() => {
+  changeStoreLink(sirensEpicShortLabel);
 });
