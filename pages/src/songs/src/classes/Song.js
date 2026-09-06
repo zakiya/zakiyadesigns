1a6d044a1790a73673ac60bf4ee0faa60fa3f151
zakiya <915352+zakiya@users.noreply.github.co>
2026-09-06 08:20:57 -0700

    Delete the pages folder. It's obsolete now.

--- pages/src/songs/src/classes/Song.js
+++
@@ -1,9 +0,0 @@
-const Song = class {
-  constructor(attributes) {
-    this.body = attributes.body.processed;
-    this.title = attributes.title;
-    this.number = attributes.field_number;
-  }
-};
-
-export default Song
